90310285cacefda773724028087f06fcefc21b3e
eric <derruette@yahoo.fr>
2016-06-19 01:33:22 +0200

    all CRUD DAO operation for the customer

---
+++ primems/src/test/java/com/friquerette/primems/dao/AbstractTest.java
@@ -0,0 +1,36 @@
+package com.friquerette.primems.dao;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import junit.framework.TestCase;
+
+@TransactionConfiguration(defaultRollback = true)
+@TestExecutionListeners(DependencyInjectionTestExecutionListener.class)
+@ContextConfiguration(classes = HibernateConfigurationTest.class)
+// @TestPropertySource("classpath:test.properties")
+@ComponentScan(basePackages = { "com.friquerette.primems.service", //
+		"com.friquerette.primems.dao", //
+		"com.friquerette.primems.entity" })
+// @TestPropertySource(properties = "assets.file=classpath:test.properties")
+public abstract class AbstractTest extends TestCase {
+
+	private Date startDate = null;
+
+	@Before
+	public void executedBeforeEach() {
+		startDate = new Date();
+	}
+
+	@After
+	public void executedAfterEach() {
+		// TODO : clean data properly (not throw SQL Script...)
+	}
+}
---
+++ primems/src/test/java/com/friquerette/primems/dao/CustomerDaoTest.java
@@ -0,0 +1,75 @@
+package com.friquerette.primems.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.friquerette.primems.entity.Customer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CustomerDaoTest extends AbstractTest {
+
+	@Autowired
+	private CustomerDao customerDao;
+
+	@Test
+	public void testReadCustomerById() {
+		Customer customer = customerDao.findById(1L);
+		assertNotNull(customer);
+	}
+
+	@Test
+	public void testUpdateCustomer() {
+		// Create Customer
+		Customer customer = getNewCustomer();
+		assertEquals("Toto", customer.getFirstName());
+		customerDao.create(customer);
+		// Update Customer
+		customer.setFirstName("Jean2");
+		customerDao.update(customer);
+		// Read and check
+		customer = customerDao.findById(customer.getId());
+		assertNotNull(customer);
+		assertEquals("Jean2", customer.getFirstName());
+	}
+
+	@Test
+	public void testCreateCustomer() {
+		Customer customer = getNewCustomer();
+		customerDao.create(customer);
+		assertNotNull(customer.getId());
+		customer = customerDao.findById(customer.getId());
+		assertEquals("Toto", customer.getFirstName());
+	}
+
+	@Test
+	public void testDeleteCustomer() {
+		Customer customer = getNewCustomer();
+		customerDao.create(customer);
+		Long id = customer.getId();
+		assertNotNull(id);
+		customer = customerDao.findById(id);
+		assertNotNull(customer);
+		customerDao.delete(customer);
+		customer = customerDao.findById(id);
+		assertNull(customer);
+	}
+
+	/**
+	 * Return a new customer
+	 * 
+	 * @return a customer
+	 */
+	private Customer getNewCustomer() {
+		Customer customer = new Customer();
+		customer.setActive(true);
+		customer.setFirstName("Toto");
+		customer.setLastName("Martin");
+		customer.setLogin("Martin");
+		customer.setEmail("test@yopmail.com");
+		customer.setPassword("pwd");
+		return customer;
+	}
+
+}
